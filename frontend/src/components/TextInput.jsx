4534c774bfd26cab2c17662dd47bcf97c231e551
Dhruv Khanna <dk1g22@soton.ac.uk>
2024-12-26 19:29:22 +0000

    Add navigate to search page

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function TextInput({username}) {
   const [prompt, setInputValue] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
+    // TODO: Work from here for receiving the response from the backend about fetched products from vendors
+    // Retrieve the input data from the backend, and redirect the user to the search results page
     try {
-      const response = await fetch('http://localhost:5000/product_text', {
+      const response = await fetch('http://localhost:7071/product_text', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -15,6 +19,7 @@ function TextInput({username}) {
 
       const result = await response.json();
       console.log('Response from backend:', result);
+      navigate('/search');
     } catch (error) {
       console.error('Error sending input data:', error);
     }
