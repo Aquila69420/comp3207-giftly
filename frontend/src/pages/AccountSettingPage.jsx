311feb4a7be38b2809f79c282cca06d29a5d8132
Harry Leslie <88256796+Harry-Leslie@users.noreply.github.com>
2024-12-29 13:53:30 +0000

    adding the product type search

--- frontend/src/pages/AccountSettingPage.jsx
+++ frontend/src/pages/AccountSettingPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from "../styles/accountSettingPage.module.css"
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 
 export default function AccountSettingPage() {
+    const navigate = useNavigate()
+    const handleBack = (e) => {
+        navigate("/")
+    }
   return (
-    <div>AccountSettingPage</div>
+    <div className={styles.backbutton} onClick={handleBack}>
+    <IoMdArrowRoundBack size={25} />
+  </div>
+
   )
 }
